47e30e382505678c7c744b2a03b3d01e80f095da
Kirill <kirilldrozd12@gmail.com>
2022-02-13 23:44:32 +0300

    Тут вроде как происходит парс с помощью swiftyJSON и это работает. Получилось залупенить все title в array и работать с ними дальше. ЭТА ВЕТКА БУДЕТ ТУПИКОВОЙ

--- Recipes/Conrtollers/ViewController.swift
+++ Recipes/Conrtollers/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      downloadJSON()
+
     }
 
+  var araayElemnts = [String]()
+
+  private func downloadJSON(){
+    let url = URL(string: "https://api.spoonacular.com/recipes/complexSearch?cuisine=Indian&apiKey=d0d03c927c034a62af87be5f7ab71334&number=113")
+    AF.request(url!, method: .get).validate().responseJSON { [self] response in
+      switch response.result {
+
+      case .success(let value):
+        let json = JSON(value)
+
+        let test = json["results"][0]["image"] // адрес фотки
+        print("JSON == \(test)") // вывод адреса фотки
+
+        let titles = json["results"].arrayValue.map{$0["title"].stringValue} // получение всех titles
+        let id = json["results"].arrayValue.map{$0["id"].stringValue}       // получение всез id
+
+        // add array
+        araayElemnts = titles
+
+      case .failure(let error):
+        print("Ошибка == \(error)")
+      }
+
+      print(araayElemnts.count) // тут приходит 100
+      print(araayElemnts)      // тут приходят все title
+
+
+    }
+  }
 
 }
+
 
