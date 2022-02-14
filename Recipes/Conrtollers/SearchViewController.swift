d2a480fdf7b07de337e15d2cb21fa0b8ec77a727
Mikhail Bobrov <bobermiha@yandex.ru>
2022-02-14 20:45:08 +0500

    Custom flowlayout

--- Recipes/Conrtollers/SearchViewController.swift
+++ Recipes/Conrtollers/SearchViewController.swift
@@ -24,6 +24,21 @@ class SearchViewController: UICollectionViewController {
     print("Hello world")
 
   }
+    
+    func generateGridLayout() -> UICollectionViewLayout {
+        let padding: CGFloat = 20
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                                             heightDimension: .fractionalHeight(1)))
+        let groupe = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/4)), subitem: item, count: 2)
+        groupe.interItemSpacing = .fixed(padding)
+        groupe.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: padding, bottom: 0, trailing: padding)
+        
+        let section = NSCollectionLayoutSection(group: groupe)
+        section.interGroupSpacing = padding
+        section.contentInsets = NSDirectionalEdgeInsets(top: padding, leading: 0, bottom: padding, trailing: 0)
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
 
 }
 
